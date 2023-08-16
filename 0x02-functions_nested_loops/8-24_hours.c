e2970599c4f8d689ffa52b4f16f78865fafb5594
Emmanuel <wizzarddguru@gmail.com>
2023-08-16 03:53:43 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ void jack_bauer(void)
 	{
 	for (b = 0; b <= 9; b++)
 	{
-	if ((a <= 1  && b <= 9) || (a <= 2&& b <= 3))
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 	{
 	for (c = 0; c <= 5; c++)
 	{
