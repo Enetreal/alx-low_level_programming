d6d9d2428ba5e32abcb7452cee528bd7c4abeb4b
Emmanuel <wizzarddguru@gmail.com>
2023-08-23 19:45:47 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,8 +8,9 @@
   * Return: less than 0 if s1 is less than s2, 0 if they're equal, 
   * more than 0 if s1 is greater than s2 
   */ 
- int _strcmp(char *s1, char *s2) 
- { 
+
+int _strcmp(char *s1, char *s2) 
+{ 
          while (*s1 == *s2) 
          { 
                  if (*s1 == '\0') 
@@ -20,4 +21,4 @@
                  s2++; 
          } 
          return (*s1 - *s2); 
- }
+}
