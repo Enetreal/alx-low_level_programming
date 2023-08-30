57733768f7426393e9867acd2fcfa96ab10289be
Emmanuel <wizzarddguru@gmail.com>
2023-08-30 03:32:23 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,18 @@
 /**
  * _puts_recursion - prints a string
  * @s: pointer block of memory to fill
- * Return: void
+ * Return: Always 0.
  */
 
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') 
+    if (*s == '\0')
     {
         _putchar('\n');
         return;
     }
     
-    _putchar(*s);
+    _putchar (*s);
     _puts_recursion(s + 1);
+
 }
