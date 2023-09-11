3857d4ec27c1666f560f328f6a1a1e29d0ee0cac
Emmanuel <wizzarddguru@gmail.com>
2023-09-11 14:41:14 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the name of the file
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
