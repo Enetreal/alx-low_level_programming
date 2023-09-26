28cbc167808d1b5175bbbad4ef5001900d06e240
Emmanuel <wizzarddguru@gmail.com>
2023-09-26 06:12:42 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t node_count = 1;
 
-	/* return ) as number of nodes when h is null */
+	/* return  as number of nodes when h is null */
 	if (h == NULL)
 		return (0);
 
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - Counts the number of elements in a linked list.
+ *
+ * @h: Pointer to the first node of the list
+ *
+ * Return: Number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0; 
+
+	while (h != NULL)
+	{
+		count++;
+
+		h = h->next;
+	}
+
+	return (count);
+}
