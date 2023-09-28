32f480157ca8fbb0f30d155c7d1720c434b341a2
Emmanuel <wizzarddguru@gmail.com>
2023-09-28 15:25:07 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,66 +1,29 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a character to an integer
- * @c: The character to convert
- * Return: The converted integer
- */
-unsigned int _atoi(char c)
-{
-	return ((unsigned int)c - '0');
-}
-
-/**
- * _strlen - Gets the lenght of a string
- * @str: The input string
- * return: The string lenght
- */
-unsigned int _strlen(const char *str)
-{
-	unsigned int index = 0;
-
-	while (str[index] != '\0')
-		index++;
-	return (index);
-}
-
-/**
- * binary_to_unit - Converts a binary number to an unsigned int
- * @b: The string containing '0' and '1' characters
- * Return: The converted number, or 0 if @b contains 
- * non-binary characters or is NULL
+ * binary_to_uint -converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return:converted number, or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int index;
-	unsigned int result = 0, base2 = 1, num = 0;
+	int i;
+	unsigned int j;
 
-	/* check if b is NULL */
-	if (b == NULL)
+	j = 0;
+	if (!b)
 		return (0);
-#ifdef DEBUG
-	printf("String is %s, and length is %u. \n", b, _strlen(b));
-#endif
-
-	for (index = _strlen(b) - 1; index >= 0; index--)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		num = _atoi(b[index]);
-
-#ifdef DEBUG
-		printf("Number is %u, index is %i, and base is %u. \n\n", num, index, base2);
-#endif
-
-	       /* if number is not 0 or 1 return 0 */	
-	       if (num != 0 && num != 1)
-	       return (0);
-
-	       result += num * base2;
-	       base2 *= 2;
-
-#ifdef DEBUG
-	       printf("Result is %u. \n", result);
-#endif
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
-
-	return (result);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
