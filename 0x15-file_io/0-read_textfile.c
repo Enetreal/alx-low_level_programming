2bf563921ab79bcc3ed123414b2f465a251056e3
Emmanuel <wizzarddguru@gmail.com>
2023-10-04 06:04:39 -0700

    1-create_file.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h> /* Include for open, read, write, and close functions */
+#include <fcntl.h> /* Include for file-related constants (e.g., 0_RDONLY) */
+
+/**
+ * read_textfile - Reads a text file amnd prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ * 	 O/w - the actual number of bytes the function can read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char * buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	if (o == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	r = read(0, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		close(o);
+		return (0);
+	}
+
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1 || w != r)
+
+	{
+		free(buffer);
+		close(o);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+
+	return (w);
+}
