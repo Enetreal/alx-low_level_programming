019a0239e841e9fa996ebcf6d3d83128b8420bca
Emmanuel <wizzarddguru@gmail.com>
2023-10-04 06:24:24 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,12 +9,12 @@
  * @letters: The number of letters the function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- * 	 O/w - the actual number of bytes the function can read and print.
+ * O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
-	char * buffer;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
