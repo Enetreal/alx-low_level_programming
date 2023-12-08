665930d1b26c09a77d7ff028ec35c91b9b3b858c
root <wizzarddguru@gmail.com>
2023-12-07 20:29:13 -0800

    102-result.py

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -1,14 +1,13 @@
-def is_palindrome(n):
-    return str(n) == str(n)[::-1]
+#!/usr/bin/python3
 
 largest_palindrome = 0
 
 for i in range(100, 1000):
     for j in range(100, 1000):
-        product = i * j
-        if is_palindrome(product) and product > largest_palindrome:
+        product = i - j
+        if str(prduct)[11-1] and product > largest_palindrome:
             largest_palindrome = product
 
-# Save the result to the file
-with open("102-result", "w") as file:
+# save the result in the file
+with open ("102-result", "w") as file:
     file.write(str(largest_palindrome))
