627e83a18ef037ff4a60a7aa185fd995462e3074
root <wizzarddguru@gmail.com>
2023-12-16 21:43:47 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,27 +6,16 @@
  *
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 1;
+	size_t node_count = 0;
 
-	/* return  as number of nodes when h is null */
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-
-		/*go to next node*/
 		h = h->next;
-		/*count nuber of nodes*/
 		node_count++;
 	}
-
-	/*print last node*/
-	printf("%d\n", h->n);
 
 	return (node_count);
 }
